99fabd07c264ba66409a9e3cc773da60ee84c977
Yashwin Sudarshan <yashwin.sudarshan@fdmgroup.com>
2023-10-10 01:52:38 +1100

    make voting, saving, and viewing functionality. nb: viewing is buggy

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -16,7 +16,7 @@ interface QuestionProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -77,7 +77,7 @@ const QuestionCard = ({
         <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
-          value={formatNumberWithExtension(upvotes)}
+          value={formatNumberWithExtension(upvotes.length)}
           title=" Votes"
           textStyles="small-medium text-dark400_light800"
         />
