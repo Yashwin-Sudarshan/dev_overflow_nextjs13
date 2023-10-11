bc4e4b3c85dbeffd531b92efb2ddc61be71cf1be
Yashwin Sudarshan <yashwin.sudarshan@fdmgroup.com>
2023-10-11 12:50:38 +1100

    make edit delete question and answer functionality

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import RenderTag from "../shared/RenderTag";
 import Metric from "../shared/Metric";
 import { formatNumberWithExtension, getTimeStamp } from "@/lib/utils";
+import { SignedIn } from "@clerk/nextjs";
+import EditDeleteAction from "../shared/EditDeleteAction";
 
 interface QuestionProps {
   _id: string;
@@ -34,6 +36,7 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionProps) => {
+  const showActionButtons = clerkId && clerkId === author.clerkId;
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div
@@ -57,7 +60,11 @@ const QuestionCard = ({
           </Link>
         </div>
 
-        {/** If signed in add edit delete actions */}
+        <SignedIn>
+          {showActionButtons && (
+            <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
+          )}
+        </SignedIn>
       </div>
 
       <div className="mt-3.5 flex flex-wrap gap-2">
